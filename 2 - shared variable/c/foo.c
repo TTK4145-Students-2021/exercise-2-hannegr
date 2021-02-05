88de54f321753aeef6bae8af64f59ef465dfce96
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-02-05 12:03:06 +0100

    synced in C using mutex

--- 2 - shared variable/c/foo.c
+++ 2 - shared variable/c/foo.c
@@ -1,21 +1,37 @@
 #include <pthread.h>
 #include <stdio.h>
+//mutexe or semaphores? - Here I choose mutex, because we only have two threads anyways, so it will work out allright to lock and unlock. I could also have used a binary semaphore. 
 
 int i = 0;
+pthread_mutex_t mutex; 
 
 // Note the return type: void*
 void* incrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
 	// TODO: sync access to i
-	i++;
+    //if(pthread_mutex_trylock(&mutex)){
+    //    i++; 
+    //    pthread_mutex_unlock(&mutex); 
+    //}
+    pthread_mutex_lock(&mutex);
+    i++;
+    pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
-    for (int j = 0; j < 1000000; j++) {
+    for (int j = 0; j < 500000; j++) {//999998
+        //I change the number here to 988, so that i should become -2.
 	// TODO: sync access to i
-	i--;
+    //if(pthread_mutex_trylock(&mutex)){
+    //    i--; 
+    //    pthread_mutex_unlock(&mutex); 
+   // }
+	//i--;
+    pthread_mutex_lock(&mutex);
+    i--;
+    pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
