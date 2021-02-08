a43596bddec6951d12b378476527710ef6da6d33
Hanne-Grete Alvheim <hannegga@stud.ntnu.no>
2021-02-08 10:31:29 +0100

    worked with shared variables in go, not done yet

--- 2 - shared variable/go/foo.go
+++ 2 - shared variable/go/foo.go
@@ -1,10 +1,23 @@
 // Use `go run foo.go` to run your program
+//THESE COMMENTS ARE FOR ME
+//goroutine: lightweight thread managed by go runtime If you make a function and run go that function and another function,
+//both will run at the same time.
+//channel: send and receive values. Data flows in the direction of the values. ch <- v: v is sent to channel ch. v:= <- ch: recieve from ch and
+//assign value to v.
+//create channel by: ch := make(chan int)
+//Channels can be buffered using ch := make(chan int, number), will then block when number of messages is bigger than number.
+//close(ch): close channel
+//select: blocks until one of its cases can run, then run that case. Chooses a random if multiple are ready.
+//select{
+//		*do stuff
+//      }
 
 package main
 
 import (
 	"fmt"
 	"runtime"
+	"sync"
 )
 
 func number_server(add <-chan int, sub <-chan int, read chan<- int) {
@@ -13,33 +26,67 @@ func number_server(add <-chan int, sub <-chan int, read chan<- int) {
 	// This for-select pattern is one you will become familiar with...
 	for {
 		select {
-        }
+		case num := <-add:
+			number += num
+			//ch1 <- num
+
+		case num2 := <-sub:
+			number -= num2
+		//ch2 <- num
+
+		case read <- number:
+			return
+
+		}
 	}
 }
+
+//Using shared variable synchronization is possible, but not the idiomatic approach in Go.
+//You should instead create a server that is responsible for its own data, select{}s messages, and perform different actions on its data when it receives a corresponding message.
+//The server should have three actions it can perform: Increment, decrement, and read (or "get"). Two other goroutines should send the increment and decrement requests to the server,
+//and main should read out the final value after these two goroutines are done.
 
 func incrementer(add chan<- int, finished chan<- bool) {
 	for j := 0; j < 1000000; j++ {
 		add <- 1
+		finished <- true
 	}
-	//TODO: signal that the goroutine is finished
+	//TODO: signal that the goroutine is finished¨
+	//if(finished == true):
+	//	close(finished)
+
 }
 
 func decrementer(sub chan<- int, finished chan<- bool) {
 	for j := 0; j < 1000000+1; j++ {
 		sub <- 1
+		finished <- true
 	}
 	//TODO: signal that the goroutine is finished
+	//if(finished == true):
+	//	close(finished)
+
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-
 	// TODO: Construct the remaining channels
-	read := make(chan int)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	ch3 := make(chan bool)
+	ch4 := make(chan bool)
 
 	// TODO: Spawn the required goroutines
+	read := make(chan int)
+	go number_server(ch1, ch2, read)
+	go incrementer(ch1, ch3)
+	go decrementer(ch2, ch4)
 
 	// TODO: block on finished from both "worker" goroutines
+	var wg sync.WaitGroup
+	wg.Wait()
+	close(ch3)
+	close(ch4)
 
 	fmt.Println("The magic number is:", <-read)
 }
